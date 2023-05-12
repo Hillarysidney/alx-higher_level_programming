f8a657db6c654446df895477383fb6567e7b4c7b
Hillarysidney <hillarysidney4@gmail.com>
2023-05-12 03:38:08 -0700

    Hot fix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    n = 0
+    nancy = 0
     new_strin = ""
     for j in str:
-        if n != n:
+        if nancy != n:
             new_strin += j
-        n += 1
+        nancy += 1
     return new_strin
