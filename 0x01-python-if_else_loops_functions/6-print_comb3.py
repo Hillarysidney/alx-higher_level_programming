4932248fafd4fb255cef7ecd6df717423979cd33
Hillarysidney <hillarysidney4@gmail.com>
2023-05-12 03:46:44 -0700

    Hot fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for my_numbers in range(100):
-    if int(my_numbers / 10) != my_numbers % 10 and int(my_numbers / 10) < my_numbers % 10:
-        print("{}{}".format(int(my_numbers / 10), my_numbers % 10), end="")
-        if (my_numbers != 89):
+for numbas in range(100):
+    if int(numbas / 10) != numbas % 10 and int(numbas / 10) < numbas % 10:
+        print("{}{}".format(int(numbas / 10), numbas % 10), end="")
+        if (numbas != 89):
             print(", ", end="")
 print("")
